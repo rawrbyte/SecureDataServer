be2b707988702e810adba69acc94b2ec9de91e78
Vincent Tran <vincent.tran@tamu.edu>
2017-10-25 18:12:31 -0500

    updated parser to handle whitespace and newlines

--- parser.py
+++ parser.py
@@ -5,27 +5,27 @@ import sys
 from lark import Lark
 
 grammar = Lark(r"""
-	prog		: "as principal " p " password " s " do \n " cmd
-	cmd 		: "exit \n" 
-				| "return " expr " \n"
-				| prim_cmd " \n " cmd
+	prog		: "as" "principal" p "password" s "do" "\n" cmd
+	cmd 		: "exit" "\n" 
+				| "return" expr "\n"
+				| prim_cmd "\n" cmd
 	expr		: value 
 				| "[]" 
 				| "{" fieldvals "}"
-	fieldvals	: x " = " value
-				| x " = " value " ," fieldvals
+	fieldvals	: x "=" value
+				| x "=" value "," fieldvals
 	value		: x
-				| x " . " y
+				| x "." y
 				| s 
-	prim_cmd	: "create principal " p " " s
-				| "change password " p " " s
-				| "set " x " = " expr
-				| "append to " x " with " expr
-				| "local " x " = " expr
-				| "foreach " y " in " x " replacewith " expr
-				| "set delegation " tgt " " q " " right " -> " p
-				| "delete delegation " tgt " " q " " right " -> " p
-				| "default delegator = " p
+	prim_cmd	: "create" "principal" p s
+				| "change" "password" p s
+				| "set" x "=" expr
+				| "append" "to" x "with" expr
+				| "local" x "=" expr
+				| "foreach" y "in" x "replacewith" expr
+				| "set" "delegation" tgt q right "->" p
+				| "delete" "delegation" tgt q right "->" p
+				| "default" "delegator" "=" p
 	tgt 		: "all"
 				| x
 	right 		: "read"
@@ -41,9 +41,8 @@ grammar = Lark(r"""
 	%import common.WORD
 	%import common.ESCAPED_STRING
 	%import common.WS
+	%ignore /[ \t\f\r]+/
 	
 	""", start='prog')
 
-# print(grammar.parse("as principal admin password \"B0BPWxxd\" do \n set z = \"bobs string\""))
-
-print(grammar.parse("as principal bob password \"B0BPWxxd\" do \n set z = \"bobs string\" \n set x = \"another string\" \n return x \n"))
+print(grammar.parse('as principal bob password "B0BPWxxd" do \n set z = "bobs string" \n set x = "another string" \n return x \n').pretty())
