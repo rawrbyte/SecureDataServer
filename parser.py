5af8a7becdaeed8aca6ae41379e9cb0192d172be
rawrbyte <rawrbyte@tamu.edu>
2017-10-26 23:41:28 -0500

    new file

--- parser.py
+++ parser.py
@@ -3,46 +3,156 @@
 import sys
 
 from lark import Lark
+from lark import Transformer
+
+class GrammarTransformer(Transformer):
+	def prog0(self, param):
+		principal = param[0].children[0]
+		user = param[1].children[0]
+		print(param)
+		return "param"
+
+	def cmd0(self, param):
+		return "cmd0"
+
+	def cmd1(self, param):
+		return "cmd1"
+
+	def cmd2(self, param):
+		return "cmd2"
+
+	def expr0(self, param):
+		return "expr0"
+
+	def expr1(self, param):
+		return "expr1"
+
+	def expr2(self, param):
+		return "expr2"
+
+	def fieldvals0(self, param):
+		return "fieldvals0"
+
+	def fieldvals1(self, param):
+		return "fieldvals1"
+
+	def value0(self, param):
+		return "value0"
+
+	def value1(self, param):
+		return "value1"
+
+	def value2(self, param):
+		return "value2"
+
+	def prim_cmd0(self, param):
+		return "prim_cmd0"
+
+	def prim_cmd1(self, param):
+		return "prim_cmd1"
+
+	def prim_cmd2(self, param):
+		#print(param)
+		#print('--------------------------------')
+		return "prim_cmd2"
+
+	def prim_cmd3(self, param):
+		return "prim_cmd3"
+
+	def prim_cmd4(self, param):
+		return "prim_cmd4"
+
+	def prim_cmd5(self, param):
+		return "prim_cmd5"
+
+	def prim_cmd6(self, param):
+		return "prim_cmd6"
+
+	def prim_cmd7(self, param):
+		return "prim_cmd7"
+
+	def prim_cmd8(self, param):
+		return "prim_cmd8"
+
+	def tgt0(self, param):
+		return "tgt0"
+
+	def tgt1(self, param):
+		return "tgt1"
+
+	def right0(self, param):
+		return "right0"
+
+	def right1(self, param):
+		return "right1"
+
+	def right2(self, param):
+		return "right2"
+
+	def right3(self, param):
+		return "right3"
+
 
 grammar = Lark(r"""
-	prog		: "as" "principal" p "password" s "do" "\n" cmd
-	cmd 		: "exit" "\n" 
-				| "return" expr "\n"
-				| prim_cmd "\n" cmd
-	expr		: value 
-				| "[]" 
-				| "{" fieldvals "}"
-	fieldvals	: x "=" value
-				| x "=" value "," fieldvals
-	value		: x
-				| x "." y
-				| s 
-	prim_cmd	: "create" "principal" p s
-				| "change" "password" p s
-				| "set" x "=" expr
-				| "append" "to" x "with" expr
-				| "local" x "=" expr
-				| "foreach" y "in" x "replacewith" expr
-				| "set" "delegation" tgt q right "->" p
-				| "delete" "delegation" tgt q right "->" p
-				| "default" "delegator" "=" p
-	tgt 		: "all"
-				| x
-	right 		: "read"
-				| "write"
-				| "append"
-				| "delegate"
-	p			: WORD
-	q			: WORD
+	prog		: "as" "principal" p "password" s "do" "\n" cmd "***"	-> prog0
+	cmd 		: "exit" "\n" 						-> cmd0
+				| "return" expr "\n"				-> cmd1
+				| prim_cmd "\n" cmd 				-> cmd2
+	expr		: value							-> expr0
+				| "[]" 						-> expr1
+				| "{" fieldvals "}"				-> expr2
+	fieldvals	: x "=" value						-> fieldvals0
+				| x "=" value "," fieldvals 			-> fieldvals1
+	value		: x							-> value0
+				| x "." y					-> value1
+				| s 						-> value2
+	prim_cmd	: "create" "principal" p s 				-> prim_cmd0
+				| "change" "password" p s 			-> prim_cmd1
+				| "set" x "=" expr 				-> prim_cmd2
+				| "append" "to" x "with" expr 			-> prim_cmd3
+				| "local" x "=" expr 				-> prim_cmd4
+				| "foreach" y "in" x "replacewith" expr 	-> prim_cmd5
+				| "set" "delegation" tgt q right "->" p 	-> prim_cmd6
+				| "delete" "delegation" tgt q right "->" p 	-> prim_cmd7
+				| "default" "delegator" "=" p 			-> prim_cmd8
+	tgt 		: "all"							-> tgt0
+				| x 						-> tgt1
+	right 		: "read" 						-> right0
+				| "write" 					-> right1
+				| "append" 					-> right2
+				| "delegate" 					-> right3
+	p			: CNAME
+	q			: CNAME
 	s 			: ESCAPED_STRING
-	x			: WORD
-	y			: WORD
+	x			: CNAME
+	y			: CNAME
 
 	%import common.WORD
+	%import common.CNAME
 	%import common.ESCAPED_STRING
 	%import common.WS
 	%ignore /[ \t\f\r]+/
 	
 	""", start='prog')
 
-print(grammar.parse('as principal bob password "B0BPWxxd" do \n set z = "bobs string" \n set x = "another string" \n return x \n').pretty())
+
+def messageHandler(text):
+	tree = grammar.parse(text)
+	GrammarTransformer.transform(tree)
+
+
+
+#--------------------------------------------------------------------------------
+
+
+
+print(grammar.parse('as principal bob password "B0BPWxxd" do \n set z = "bobs string" \n set x = "another string" \n return x \n ***').pretty())
+
+print('\n\n\n\n')
+
+tree = grammar.parse('as principal bob password "B0BPWxxd" do \n set z = "bobs string" \n set x = "another string" \n return x \n ***')
+GrammarTransformer().transform(tree)
+
+#tree = grammar.parse('as principal bob password "B0BPWxxd" do \n set x = "test string" \n set delegation x mike read -> bob \n return x \n ***')
+#GrammarTransformer().transform(tree)
+
