8a5628eee274dc05cb34a0ffa4861156952a2e82
rawrbyte <rawrbyte@tamu.edu>
2017-10-29 14:06:36 -0500

    added reserveKeyword method

--- parser.py
+++ parser.py
@@ -9,118 +9,119 @@ class GrammarTransformer(Transformer):
 	def prog0(self, param):
 		principal = param[0].children[0]
 		user = param[1].children[0]
+		#print(param)
+		#return param
 		print(param)
-		return "param"
 
 	def cmd0(self, param):
-		return "cmd0"
+		print(param)
 
 	def cmd1(self, param):
-		return "cmd1"
+		print(param)
 
 	def cmd2(self, param):
-		return "cmd2"
-
+		print(param)
+	
 	def expr0(self, param):
-		return "expr0"
+		print(param)
 
 	def expr1(self, param):
-		return "expr1"
+		print(param)
 
 	def expr2(self, param):
-		return "expr2"
+		print(param)
 
 	def fieldvals0(self, param):
-		return "fieldvals0"
+		print(param)
 
 	def fieldvals1(self, param):
-		return "fieldvals1"
+		print(param)
 
 	def value0(self, param):
-		return "value0"
+		print(param)
 
 	def value1(self, param):
-		return "value1"
+		print(param)
 
 	def value2(self, param):
-		return "value2"
+		print(param)
 
 	def prim_cmd0(self, param):
-		return "prim_cmd0"
+		print(param)
 
 	def prim_cmd1(self, param):
-		return "prim_cmd1"
+		print(param)
 
 	def prim_cmd2(self, param):
 		#print(param)
 		#print('--------------------------------')
-		return "prim_cmd2"
+		print(param)
 
 	def prim_cmd3(self, param):
-		return "prim_cmd3"
+		print(param)
 
 	def prim_cmd4(self, param):
-		return "prim_cmd4"
+		print(param)
 
 	def prim_cmd5(self, param):
-		return "prim_cmd5"
+		print(param)
 
 	def prim_cmd6(self, param):
-		return "prim_cmd6"
+		print(param)
 
 	def prim_cmd7(self, param):
-		return "prim_cmd7"
+		print(param)
 
 	def prim_cmd8(self, param):
-		return "prim_cmd8"
+		print(param)
 
 	def tgt0(self, param):
-		return "tgt0"
+		print(param)
 
 	def tgt1(self, param):
-		return "tgt1"
+		print(param)
 
 	def right0(self, param):
-		return "right0"
+		print(param)
 
 	def right1(self, param):
-		return "right1"
+		print(param)
 
 	def right2(self, param):
-		return "right2"
+		print(param)
 
 	def right3(self, param):
-		return "right3"
+		print(param)
 
 
 grammar = Lark(r"""
-	prog		: "as" "principal" p "password" s "do" "\n" cmd "***"	-> prog0
-	cmd 		: "exit" "\n" 						-> cmd0
-				| "return" expr "\n"				-> cmd1
-				| prim_cmd "\n" cmd 				-> cmd2
-	expr		: value							-> expr0
-				| "[]" 						-> expr1
-				| "{" fieldvals "}"				-> expr2
-	fieldvals	: x "=" value						-> fieldvals0
-				| x "=" value "," fieldvals 			-> fieldvals1
-	value		: x							-> value0
-				| x "." y					-> value1
-				| s 						-> value2
-	prim_cmd	: "create" "principal" p s 				-> prim_cmd0
-				| "change" "password" p s 			-> prim_cmd1
-				| "set" x "=" expr 				-> prim_cmd2
-				| "append" "to" x "with" expr 			-> prim_cmd3
-				| "local" x "=" expr 				-> prim_cmd4
-				| "foreach" y "in" x "replacewith" expr 	-> prim_cmd5
-				| "set" "delegation" tgt q right "->" p 	-> prim_cmd6
-				| "delete" "delegation" tgt q right "->" p 	-> prim_cmd7
-				| "default" "delegator" "=" p 			-> prim_cmd8
-	tgt 		: "all"							-> tgt0
-				| x 						-> tgt1
-	right 		: "read" 						-> right0
-				| "write" 					-> right1
-				| "append" 					-> right2
-				| "delegate" 					-> right3
+	prog		: "as" "principal" p "password" s "do" "\n" cmd "***"		-> prog0
+	cmd 		: "exit" "\n" 												-> cmd0
+				| "return" expr "\n"										-> cmd1
+				| prim_cmd "\n" cmd 										-> cmd2
+	expr		: value														-> expr0
+				| "[]" 														-> expr1
+				| "{" fieldvals "}"											-> expr2
+	fieldvals	: x "=" value												-> fieldvals0
+				| x "=" value "," fieldvals 								-> fieldvals1
+	value		: x															-> value0
+				| x "." y													-> value1
+				| s 														-> value2
+	prim_cmd	: "create" "principal" p s 									-> prim_cmd0
+				| "change" "password" p s 									-> prim_cmd1
+				| "set" x "=" expr 											-> prim_cmd2
+				| "append" "to" x "with" expr 								-> prim_cmd3
+				| "local" x "=" expr 										-> prim_cmd4
+				| "foreach" y "in" x "replacewith" expr 					-> prim_cmd5
+				| "set" "delegation" tgt q right "->" p 					-> prim_cmd6
+				| "delete" "delegation" tgt q right "->" p 					-> prim_cmd7
+				| "default" "delegator" "=" p 								-> prim_cmd8
+	tgt 		: "all"														-> tgt0
+				| x 														-> tgt1
+	right 		: "read" 													-> right0
+				| "write" 													-> right1
+				| "append" 													-> right2
+				| "delegate" 												-> right3
 	p			: CNAME
 	q			: CNAME
 	s 			: ESCAPED_STRING
@@ -151,7 +152,7 @@ print(grammar.parse('as principal bob password "B0BPWxxd" do \n set z = "bobs st
 print('\n\n\n\n')
 
 tree = grammar.parse('as principal bob password "B0BPWxxd" do \n set z = "bobs string" \n set x = "another string" \n return x \n ***')
-GrammarTransformer().transform(tree)
+print(GrammarTransformer().transform(tree))
 
 #tree = grammar.parse('as principal bob password "B0BPWxxd" do \n set x = "test string" \n set delegation x mike read -> bob \n return x \n ***')
 #GrammarTransformer().transform(tree)
---
+++ reserve.py
@@ -0,0 +1,10 @@
+data = {'all':None, 'append':None, 'as':None, 'change':None, 'create':None, 'default':None, 'delegate':None, 'delegation':None, 'delegator':None, 'delete':None, 'do':None, 'exit':None, 'foreach':None, 'in':None, 'local':None, 'password':None, 'principal':None, 'read':None, 'replacewith':None, 'return':None, 'set':None, 'to':None, 'write':None, '***':None}
+
+def isReserveKeyword(keyword):
+    if data.__contains__(keyword):
+        return True
+    else:
+        return False
+
+print(isReserveKeyword('append'))
+print(isReserveKeyword('asof'))
